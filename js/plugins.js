4e9016f4c21bf6c3c50bb9a2c10eb2fdd5e7bc54
LenaAkrytova <oakrytov@campus.haifa.ac.il>
2016-03-15 11:28:38 +0200

    Large overnight work
    1. added form validation
    2. show-hide settings form
    3. added utils function

--- js/plugins.js
+++ js/plugins.js
@@ -2,14 +2,14 @@ var UTILS = (function () {
 
 	return {
 		addEvent: function(elem, type, handler) {
-			if(window.addEventListener) 
+			if(window.addEventListener)
 			{
-                // new browser
+			    // new browser
 	            elem.addEventListener(type, handler, false);
 	        }
 	        else if(window.attachEvent)
 	        {
-                // old style
+	            // old style
 	            elem.attachEvent('on' + type, handler);
 	        }
 	        else
@@ -17,5 +17,14 @@ var UTILS = (function () {
 	            elem['on' + type] = handler;
 	        }
 		},
+		EnsureHTTPPrefix: function (url)
+        {
+	        if ((!url.startsWith("http://")) && (!url.startsWith("http://")))
+            {
+                url = "http://" + url;
+            }
+
+            return url;
+        },
 	};
 }());
